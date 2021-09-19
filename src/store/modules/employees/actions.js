9f2b1f740f87008181edf33f9707db904b563b95
Rob van Bakel <robbakel@gmail.com>
2021-09-19 11:13:43 +0200

    Add admin host environment variable

--- src/store/modules/employees/actions.js
+++ src/store/modules/employees/actions.js
@@ -35,7 +35,7 @@ export default {
     context.commit("updateEmployee", payload)
   },
   async createNewUser(context, payload) {
-    const res = await fetch(`/createNewUser`, {
+    const res = await fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/createNewUser`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
