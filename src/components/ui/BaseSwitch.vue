ac01878ebc082642a64da90878fe4d535ae42de2
Rob van Bakel <robbakel@gmail.com>
2021-09-28 18:46:39 +0200

    Refactor BaseSwitch

--- src/components/ui/BaseSwitch.vue
+++ src/components/ui/BaseSwitch.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="switch-wrapper" ref="wrapper">
     <div class="indicator" ref="indicator"></div>
-    <div v-for="item in items" :class="['switch-control', { fixed: this.fixed }]" :key="item" @click="setActive($event, item)" ref="item">
-      {{ item }}
+
+    <div v-if="toggle">
+      <div v-for="item in [true, false]" :data-value="item" :id="id" class="switch-control toggle fixed" :key="item" @click="setActive($event, item)" ref="item">
+        <span v-if="item" class="material-icons material-icons-round">check</span>
+        <span v-else class="material-icons material-icons-round">close</span>
+      </div>
+    </div>
+
+    <div v-else>
+      <div v-for="item in items" :class="['switch-control', { fixed: this.fixed }]" :key="item" @click="setActive($event, item)" ref="item">
+        {{ item }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["items", "active", "fixed"],
+  props: {
+    toggle: {
+      type: Boolean,
+    },
+    items: {
+      type: Array,
+    },
+    active: {
+      type: String,
+    },
+    fixed: {
+      type: Boolean,
+    },
+    id: {
+      type: String
+    }
+  },
   emits: ["activeItem"],
   methods: {
     setActive(e, item) {
@@ -23,14 +49,20 @@ export default {
     },
   },
   mounted() {
-    const selectedItem = this.active
-
-    const switchItems = document.querySelectorAll(".switch-control")
-    switchItems.forEach((item) => {
-      if (item.innerText === selectedItem) {
-        item.click()
-      }
-    })
+    const switchItems = document.querySelectorAll(`#${this.id}.switch-control`)
+    if (this.toggle) {
+      switchItems.forEach((item) => {
+        if (item.attributes["data-value"].value === this.active) {
+          item.click()
+        }
+      })
+    } else {
+      switchItems.forEach((item) => {
+        if (item.innerText === this.active) {
+          item.click()
+        }
+      })
+    }
 
     setTimeout(() => {
       this.$refs.indicator.style.transition = "all 120ms ease-in-out"
