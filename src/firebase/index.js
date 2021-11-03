d866c4f3bb63b5e62a08a5aafa5c328c7ee0779e
Rob van Bakel <robbakel@gmail.com>
2021-11-03 15:13:25 +0100

    Move Firebase config to environment variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,18 +1,18 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: "scheduler-cb321.firebaseapp.com",
-  databaseURL: "https://scheduler-cb321-default-rtdb.firebaseio.com",
-  projectId: "scheduler-cb321",
-  storageBucket: "scheduler-cb321.appspot.com",
-  messagingSenderId: "1035205227793",
-  appId: "1:1035205227793:web:76e679a202d101b27108b0"
-};
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const db = firebase.firestore()
