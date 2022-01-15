778ea25c1c048637d5bb697e4d66c5927ca87394
Rob van Bakel <robbakel@gmail.com>
2022-01-15 14:48:52 +0100

    Cleanup EmptyWeek view

--- src/components/layout/EmptyWeek.vue
+++ src/components/layout/EmptyWeek.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="empty-week">
     <week-switch></week-switch>
-    <!-- <h1>Week {{ $store.getters['date/weekNumber'] }}</h1> -->
     <h5>{{ dateRange }}</h5>
     <p>
       No planning has been made for this week yet!<br />
       You can either start with an empty schedule or copy an existing week.
     </p>
     <div class="action-wrapper">
-        <div :class="['action', { transparent: showCopyWeek }]" @click="$emit('hideEmptyWeek')"><span class="material-icons material-icons-round">calendar_today</span> Create empty schedule</div>
-        <CopyWeek v-if="showCopyWeek" />
-        <div class="action" @click="showCopyWeek = true" v-else><span class="material-icons material-icons-round">event_note</span> Copy an existing week</div>
+      <div
+        :class="['action', { transparent: showCopyWeek }]"
+        @click="$emit('hideEmptyWeek')"
+      >
+        <span class="material-icons material-icons-round">calendar_today</span>
+        Create empty schedule
+      </div>
+      <CopyWeek v-if="showCopyWeek" />
+      <div class="action" @click="showCopyWeek = true" v-else>
+        <span class="material-icons material-icons-round">event_note</span> Copy
+        an existing week
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import CopyWeek from "@/components/ui/CopyWeek"
+import CopyWeek from "@/components/ui/CopyWeek";
 
 export default {
   emits: ["hideEmptyWeek"],
@@ -24,25 +32,28 @@ export default {
   data() {
     return {
       showCopyWeek: false,
-    }
+    };
   },
   computed: {
     dateRange() {
-      const dates = this.$store.getters["date/dates"]
+      const dates = this.$store.getters["date/dates"];
 
-      const formatDate = (date) => date.toLocaleDateString(this.$store.getters['settings/dateLocale'], { year: "numeric", month: "long", day: "numeric" })
+      const formatDate = (date) =>
+        date.toLocaleDateString(this.$store.getters["settings/dateLocale"], {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
 
-      return `${formatDate(dates[0])} – ${formatDate(dates[dates.length - 1])}`
+      return `${formatDate(dates[0])} – ${formatDate(dates[dates.length - 1])}`;
     },
   },
   watch: {
     $route(to) {
       if (to.name === "Planner") {
-        this.showCopyWeek = false
+        this.showCopyWeek = false;
       }
     },
   },
-}
+};
 </script>
-
-<style></style>
