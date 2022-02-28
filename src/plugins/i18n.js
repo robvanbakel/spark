65722ab9c19515ed109250ca97f5e86c6b443179
Rob van Bakel <robbakel@gmail.com>
2022-02-28 22:43:39 +0100

    Set default locale to en-US

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -4,7 +4,7 @@ import en_US from '@/locales/en-US'
 import nl_NL from '@/locales/nl-NL'
 
 const i18n = createI18n({
-  locale: 'nl-NL',
+  locale: 'en-US',
   fallbackLocale: 'en-US',
   messages: {
     'en-US': en_US,
