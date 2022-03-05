fc7a056ba6127a59bb10db6f03ced56f28a13eaa
Rob van Bakel <robbakel@gmail.com>
2022-03-05 20:34:02 +0100

    Add translation for EmployeeInfo

--- src/components/layout/EmployeeInfo.vue
+++ src/components/layout/EmployeeInfo.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="employee-info-wrapper">
-    <h3>Profile</h3>
-    <h4>Name</h4>
+    <h3>{{ $t('schedule.profile') }}</h3>
+    <h4>{{ $t('general.labels.name') }}</h4>
     <p>{{ user.firstName }} {{ user.lastName }}</p>
-    <h4>Role</h4>
+    <h4>{{ $t('general.labels.role') }}</h4>
     <p>{{ user.role }}</p>
-    <h4>Contract</h4>
-    <p>{{ user.contract }} hours ({{ user.contractType }})</p>
-    <h4>Email</h4>
+    <h4>{{ $t('general.labels.contract') }}</h4>
+    <p>{{ user.contract }} {{ $t('schedule.hours') }} ({{ user.contractType }})</p>
+    <h4>{{ $t('general.labels.email') }}</h4>
     <p>{{ user.email }}</p>
-    <h4>Phone</h4>
+    <h4>{{ $t('general.labels.phone') }}</h4>
     <p>{{ user.phone }}</p>
     <div v-if="user.notes">
-      <h4>Notes</h4>
+      <h4>{{ $t('general.labels.notes') }}</h4>
       <p>{{ user.notes }}</p>
     </div>
   </div>
