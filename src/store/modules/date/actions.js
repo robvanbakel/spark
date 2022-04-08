2d9ae3db53b7d40045916f9079270a132119cf75
Rob van Bakel <robbakel@gmail.com>
2022-04-08 20:32:38 +0200

    Reformat function to find first day of the week

--- src/store/modules/date/actions.js
+++ src/store/modules/date/actions.js
@@ -2,23 +2,10 @@ import dayjs from '@/plugins/dayjs';
 
 export default {
   async setDates(context, weekId = dayjs().weekId()) {
-    // Find first day of week
     const [year, week] = weekId.split('-');
 
-    const monday = new Date(year, 0, 1 + (week - 1) * 7);
-
-    if (monday.getDay() <= 4) {
-      monday.setDate(monday.getDate() - monday.getDay() + 1);
-    } else {
-      monday.setDate(monday.getDate() + 8 - monday.getDay());
-    }
-
-    // Create array of dates
-
-    const dates = Array.from({ length: 7 }, (element, index) => {
-      const date = new Date(monday);
-      return new Date(date.setDate(date.getDate() + index));
-    });
+    const monday = dayjs().year(year).isoWeek(week).isoWeekday(1);
+    const dates = Array.from({ length: 7 }, (element, index) => monday.add(index, 'day').toDate());
 
     // Store in global state
 
