06c17cb2e81c4018c841096d42680d2e0ac8d499
Rob van Bakel <robbakel@gmail.com>
2022-04-16 18:28:16 +0200

    Move routes to seperate file

--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import dayjs from '@/plugins/dayjs';
-
 import { auth } from '@/firebase';
 import store from '@/store';
 
-import HomeView from '@/views/HomeView.vue';
-import EmployeesView from '@/views/EmployeesView.vue';
-import PlannerView from '@/views/PlannerView.vue';
-import ScheduleView from '@/views/ScheduleView.vue';
-import AuthView from '@/views/AuthView.vue';
-import SettingsView from '@/views/SettingsView.vue';
+import routes from './routes';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: HomeView,
-      name: 'Home',
-    },
-    {
-      path: '/auth',
-      component: AuthView,
-      name: 'Auth',
-      meta: {
-        noAuth: true,
-        title: 'Login',
-      },
-    },
-    {
-      path: '/staff',
-      component: EmployeesView,
-      name: 'EmployeeList',
-      meta: {
-        admin: true,
-        title: 'Staff',
-      },
-    },
-    {
-      path: '/planner/:weekId?',
-      component: PlannerView,
-      name: 'Planner',
-      meta: {
-        admin: true,
-      },
-      async beforeEnter(to) {
-        store.dispatch('date/setDates', to.params.weekId || dayjs().weekId());
-      },
-    },
-    {
-      path: '/schedule/:weekId?',
-      component: ScheduleView,
-      name: 'Schedule',
-      async beforeEnter(to) {
-        store.dispatch('date/setDates', to.params.weekId || dayjs().weekId());
-      },
-    },
-    {
-      path: '/settings',
-      component: SettingsView,
-      name: 'Settings',
-      meta: {
-        title: 'Settings',
-      },
-    },
-  ],
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
---
+++ src/router/routes.js
@@ -0,0 +1,63 @@
+import store from '@/store';
+
+import dayjs from '@/plugins/dayjs';
+
+import HomeView from '@/views/HomeView.vue';
+import EmployeesView from '@/views/EmployeesView.vue';
+import PlannerView from '@/views/PlannerView.vue';
+import ScheduleView from '@/views/ScheduleView.vue';
+import AuthView from '@/views/AuthView.vue';
+import SettingsView from '@/views/SettingsView.vue';
+
+export default [
+  {
+    path: '/',
+    component: HomeView,
+    name: 'Home',
+  },
+  {
+    path: '/auth',
+    component: AuthView,
+    name: 'Auth',
+    meta: {
+      noAuth: true,
+      title: 'Login',
+    },
+  },
+  {
+    path: '/staff',
+    component: EmployeesView,
+    name: 'EmployeeList',
+    meta: {
+      admin: true,
+      title: 'Staff',
+    },
+  },
+  {
+    path: '/planner/:weekId?',
+    component: PlannerView,
+    name: 'Planner',
+    meta: {
+      admin: true,
+    },
+    async beforeEnter(to) {
+      store.dispatch('date/setDates', to.params.weekId || dayjs().weekId());
+    },
+  },
+  {
+    path: '/schedule/:weekId?',
+    component: ScheduleView,
+    name: 'Schedule',
+    async beforeEnter(to) {
+      store.dispatch('date/setDates', to.params.weekId || dayjs().weekId());
+    },
+  },
+  {
+    path: '/settings',
+    component: SettingsView,
+    name: 'Settings',
+    meta: {
+      title: 'Settings',
+    },
+  },
+];
