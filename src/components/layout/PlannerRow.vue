5e0ebc6e519662a7a5a5473224ae67805dcb9073
Rob van Bakel <robbakel@gmail.com>
2022-04-16 15:19:40 +0200

    Hide notes indicator when shift has not been accepted

--- src/components/layout/PlannerRow.vue
+++ src/components/layout/PlannerRow.vue
@@ -28,7 +28,7 @@
             <div class="info-wrapper">
               <div class="shift-info-header">
                 <span class="place">{{ shift.place }} </span>
-                <span class="notes material-icons material-icons-round" v-if="shift.notes">description</span>
+                <span class="notes material-icons material-icons-round" v-if="shift.notes && shift.accepted">description</span>
               </div>
               <span class="time">
                 {{ shift.start.substring(0, 2) }}:{{ shift.start.substring(2, 4) }} - {{ shift.end.substring(0, 2) }}:{{
