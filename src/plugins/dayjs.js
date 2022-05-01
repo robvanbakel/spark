7fffefa257e3fdc5e3ebe4e739e5c76e3df3684a
Rob van Bakel <robbakel@gmail.com>
2022-05-01 20:57:52 +0200

    Add dateTime format to Day.js plugin

--- src/plugins/dayjs.js
+++ src/plugins/dayjs.js
@@ -11,8 +11,16 @@ const weekId = (option, dayjsClass) => {
   };
 };
 
+const dateTime = (option, dayjsClass) => {
+  const proto = dayjsClass.prototype;
+  proto.dateTime = function () {
+    return this.format('YYYY-MM-DDTHH:mm');
+  };
+};
+
 dayjs.extend(localizedFormat);
 dayjs.extend(advancedFormat);
+dayjs.extend(dateTime);
 dayjs.extend(isoWeek);
 dayjs.extend(weekId);
 
