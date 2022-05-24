a50f433b30f8558d42c47e1fe8612984a8f74fda
Rob van Bakel <robbakel@gmail.com>
2022-05-24 20:10:44 +0200

    Refactor getUsers action

--- src/store/modules/employees/actions.js
+++ src/store/modules/employees/actions.js
@@ -3,51 +3,28 @@ import { auth, db } from '@/firebase';
 
 export default {
   async getUsers(context) {
+    const idToken = await auth.currentUser.getIdToken();
     const { uid } = auth.currentUser;
 
-    if (context.rootGetters['settings/admin'].includes(uid)) {
-      // Set global auth/admin variable to true
-      context.commit('auth/admin', true, { root: true });
+    const res = await fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/admin/db/users`, {
+      headers: { authorization: idToken },
+    });
 
-      const users = [];
+    const users = await res.json();
 
-      // If user is admin, load all users from database
-      let snapshot;
+    if (!users) return;
 
-      if (process.env.NODE_ENV === 'development') {
-        const res = await fetch(`${process.env.VUE_APP_DATA}/users`);
-        const shifts = await res.json();
-        snapshot = shifts.map(({ id, data }) => ({ id, data: () => data }));
-      } else {
-        snapshot = await db.collection('users').get();
-      }
-
-      snapshot.forEach((doc) => {
-        const user = {
-          id: doc.id,
-          ...doc.data(),
-        };
-
-        users.push(user);
-
-        if (user.id === uid) {
-          context.commit('auth/setUser', user, { root: true });
-        }
-      });
-
-      context.commit('setUsers', users);
-    } else {
-      // Set global auth/admin variable to false
-      context.commit('auth/admin', false, { root: true });
-
-      const res = await fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/admin/getUser/${uid}`);
-      const user = await res.json();
-
-      context.commit('auth/setUser', user, { root: true });
-
+    if (!Array.isArray(users)) {
+      context.commit('auth/setUser', users, { root: true });
       router.push({ name: 'Schedule' });
+      return;
     }
+
+    context.commit('auth/setUser', users.find((user) => user.id === uid), { root: true });
+    context.commit('auth/admin', true, { root: true });
+    context.commit('setUsers', users);
   },
+
   async updateUser(context, payload) {
     // Update locally
     context.commit('updateUser', payload);
