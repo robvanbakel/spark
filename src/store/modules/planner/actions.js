62348dbb612f922a512dfd3f7582b6f0628cbc34
Rob van Bakel <robbakel@gmail.com>
2022-05-30 22:36:29 +0200

    Refactor API calls for shifts

--- src/store/modules/planner/actions.js
+++ src/store/modules/planner/actions.js
@@ -1,23 +1,10 @@
-import dayjs from '@/plugins/dayjs';
-import { db, auth } from '@/firebase';
+import api from '@/utils/api';
+import format from '@/utils/format';
 
 export default {
   async getShifts(context) {
-    const idToken = await auth.currentUser.getIdToken();
-
-    const res = await fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/admin/db/shifts`, {
-      headers: { authorization: idToken },
-    });
-
-    const shifts = await res.json();
-
-    const formattedShifts = shifts.map((shift) => ({
-      ...shift,
-      from: dayjs(shift.from),
-      to: dayjs(shift.to),
-    }));
-
-    context.commit('shifts', formattedShifts);
+    const shifts = await api.get('db/shifts');
+    context.commit('shifts', format.shifts.res(shifts));
   },
   setActiveShiftId(context, payload) {
     context.commit('activeShiftId', payload);
@@ -29,78 +16,18 @@ export default {
     context.dispatch('setActiveShiftId', 'NEW');
     context.commit('newShiftPrefillData', payload);
   },
-  async saveEditShift(context, payload) {
-    const idToken = await auth.currentUser.getIdToken();
-
-    const formatData = (data) => {
-      const entries = Object.entries(data).map(([key, val]) => {
-        switch (key) {
-          case 'id':
-            return null;
-          case 'to':
-          case 'from':
-            return [key, val.dateTime()];
-          default:
-            return [key, val];
-        }
-      }).filter((v) => v);
-
-      const requestObject = Object.fromEntries(entries);
-
-      return JSON.stringify(requestObject);
-    };
-
-    // Update locally
+  saveEditShift(context, payload) {
     context.commit('updateShiftLocally', payload);
-
-    // Update DB
-    await fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/admin/db/shifts/${payload.id}`, {
-      method: payload.status === 'NEW' ? 'POST' : 'PATCH',
-      headers: {
-        authorization: idToken,
-        'Content-Type': 'application/json',
-      },
-      body: formatData(payload),
-    });
+    api[payload.status === 'NEW' ? 'post' : 'patch'](`db/shifts/${payload.id}`, format.shifts.req(payload));
   },
-  async deleteShift(context, shiftId = context.getters.activeShiftId) {
-    const idToken = await auth.currentUser.getIdToken();
-
-    // Update locally
+  deleteShift(context, shiftId = context.getters.activeShiftId) {
     context.commit('deleteShiftLocally', shiftId);
-
-    // Update DB
-    fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/admin/db/shifts/${shiftId}`, {
-      method: 'DELETE',
-      headers: { authorization: idToken },
-    });
+    api.destroy(`db/shifts/${shiftId}`);
   },
-  async copyWeek(context, payload) {
-    // Get schedule
-    const schedule = context.getters.schedules[payload.from];
-
-    // Update locally
-    context.commit('setSchedule', {
-      weekId: payload.to,
-      schedule,
-    });
-
-    // Update DB
-    db.collection('schedules')
-      .doc(payload.to)
-      .set(schedule);
-  },
-  async acceptShifts(context, shiftIds) {
-    const idToken = await auth.currentUser.getIdToken();
-
+  acceptShifts(context, shiftIds) {
     shiftIds.forEach((shiftId) => {
-      // Update locally
       context.commit('updateShiftLocally', { id: shiftId, status: 'ACCEPTED' });
-
-      // Update DB
-      fetch(`${process.env.VUE_APP_ADMIN_HOST || ''}/admin/accept/${shiftId}`, {
-        headers: { authorization: idToken },
-      });
+      api.get(`accept/${shiftId}`);
     });
   },
 };
---
+++ src/utils/format.js
@@ -0,0 +1,30 @@
+import dayjs from '@/plugins/dayjs';
+
+const shifts = {
+  req(data) {
+    const entries = Object.entries(data).map(([key, val]) => {
+      switch (key) {
+        case 'id':
+          return null;
+        case 'to':
+        case 'from':
+          return [key, val.dateTime()];
+        default:
+          return [key, val];
+      }
+    }).filter((v) => v);
+
+    const requestObject = Object.fromEntries(entries);
+
+    return JSON.stringify(requestObject);
+  },
+  res(data) {
+    return data.map((val) => ({
+      ...val,
+      from: dayjs(val.from),
+      to: dayjs(val.to),
+    }));
+  },
+};
+
+export default { shifts };
