ef0a88b97913eea419122d9cc0e319bebfbd2f3d
Rob van Bakel <robbakel@gmail.com>
2022-05-31 21:55:23 +0200

    Add randomId util

---
+++ src/utils/util.js
@@ -0,0 +1,15 @@
+const randomId = (len) => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
+
+  let string = '';
+
+  for (let i = 0; i < len; i += 1) {
+    string += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return string;
+};
+
+export default {
+  randomId,
+};
