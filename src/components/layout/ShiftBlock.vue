81609f58cdd9912b8082ceebfa59bcb2f14b6f11
Rob van Bakel <robbakel@gmail.com>
2022-06-18 14:44:15 +0200

    Refactor shift incidator icons display

--- src/components/layout/ShiftBlock.vue
+++ src/components/layout/ShiftBlock.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="shift-block" :class="{ active: this.shift, proposed: this.shift && this.shift.status !== 'ACCEPTED' }">
-    <div class="info-wrapper" v-if="this.shift" >
+    <div class="info-wrapper" v-if="this.shift">
+      <div class="icons">
+        <span v-for="icon in icons" :key="icon" class="material-symbols-outlined">{{icon}}</span>
+      </div>
       <div class="shift-info-header">
         <span class="location">{{ this.shift.location }}</span>
-        <span class="notes material-icons material-icons-round" v-if="this.shift.notes && this.shift.accepted">description</span>
       </div>
       <span class="time">
         {{ this.shift.from.format('HH:mm') }} - {{ this.shift.to.format('HH:mm') }}
@@ -17,6 +19,16 @@ export default {
   props: {
     shift: {
       type: Object,
+    },
+  },
+  computed: {
+    icons() {
+      const icons = {
+        description: this.shift?.notes,
+        question_mark: this.shift?.status === 'PROPOSED',
+      };
+
+      return Object.keys(icons).filter((icon) => icons[icon]);
     },
   },
 };
