1a9bdd655eb3c58a08ef1a4ebd3d5469ad977de6
Rob van Bakel <robbakel@gmail.com>
2022-07-02 17:07:58 +0200

    Add vue-i18n flags to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: true,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
