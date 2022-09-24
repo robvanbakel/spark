83a200a2778bcf598e10d53fd54ebe420eea8369
Rob van Bakel <robbakel@gmail.com>
2022-09-24 21:40:35 +0200

    Redirect user away from empty home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { useAuth } from '@/store';
+
+const router = useRouter();
+const authStore = useAuth();
+
+if (authStore.isAdmin) {
+  router.replace({ name: 'EmployeeList' });
+} else {
+  router.replace({ name: 'Schedule' });
+}
+</script>
+
 <template>
   <div />
 </template>
