535556a891015bfcd2bf78057ca4b9c9906efcce
Rob van Bakel <robbakel@gmail.com>
2022-09-25 22:33:12 +0200

    Refactor SideMenu

--- src/components/layout/SideMenu.vue
+++ src/components/layout/SideMenu.vue
@@ -2,28 +2,32 @@
 import { watch, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
-const props = defineProps({
-  items: {
-    type: Array,
-    required: true,
-  },
-  modelValue: {
-    type: Object,
-    default: () => {},
-  },
-});
+interface MenuItem {
+  title: string;
+  icon?: string;
+  hash: string;
+}
+
+interface Props {
+  items: MenuItem[][];
+  modelValue?: MenuItem;
+}
+
+const props = defineProps<Props>();
 
 const emit = defineEmits(["update:modelValue"]);
 
 const route = useRoute();
 const router = useRouter();
 
-const isActive = (itemHash) => route.hash === itemHash;
+const isActive = (itemHash: string) => route.hash === itemHash;
 
-const activeItem = computed(() => props.items.find((item) => item.hash === route.hash));
+const activeItem = computed(() =>
+  props.items.flat().find((item) => item.hash === route.hash)
+);
 
 if (!route.hash) {
-  router.replace({ hash: props.items[0].hash });
+  router.replace({ hash: props.items[0][0].hash });
 }
 
 watch(
@@ -40,25 +44,22 @@ watch(
     <div class="header">
       <slot />
     </div>
-    <ul>
-      <div v-for="(item, index) in items" :key="index">
-        <hr v-if="item.divider" />
-        <li v-else>
-          <router-link :to="{ hash: item.hash }">
-            <span
-              v-if="item.icon"
-              class="icon"
-              :class="
-                isActive(item.hash)
-                  ? 'material-icons material-icons-round'
-                  : 'material-symbols-outlined'
-              "
-              >{{ item.icon }}</span
-            >
-            <span :class="{ active: isActive(item.hash) }">{{ item.title }}</span>
-          </router-link>
-        </li>
-      </div>
+    <ul v-for="(group, groupIndex) in items" :key="groupIndex">
+      <li v-for="(item, itemIndex) in group" :key="itemIndex">
+        <router-link :to="{ hash: item.hash }">
+          <span
+            v-if="item.icon"
+            class="icon"
+            :class="
+              isActive(item.hash)
+                ? 'material-icons material-icons-round'
+                : 'material-symbols-outlined'
+            "
+            >{{ item.icon }}</span
+          >
+          <span :class="{ active: isActive(item.hash) }">{{ item.title }}</span>
+        </router-link>
+      </li>
     </ul>
   </nav>
 </template>
